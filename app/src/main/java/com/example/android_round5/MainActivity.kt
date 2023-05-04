004839674f85b06f0853cc6c737bda023a8eef85
ROBINRUGAN <1220045015@qq.com>
2023-05-05 02:11:18 +0800

    基本复原了导航菜单栏，中间的按钮的上移问题未解决

--- app/src/main/java/com/example/android_round5/MainActivity.kt
+++ app/src/main/java/com/example/android_round5/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.android_round5
 
 import android.os.Bundle
-import android.transition.Explode
-import android.transition.Slide
 import android.view.Gravity
-import android.view.Window
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.android_round5.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-   binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 //        在需要启动的 activity 中开启动画的特征
 //        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
 //        window.enterTransition = Explode()
@@ -35,12 +37,34 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
+        // 去除菜单项图标的着色效果
+        navView.itemIconTintList = null
+//获取中间的itemView
+        val itemView: BottomNavigationItemView = binding.navView.findViewById(R.id.navigation_upload)
+
+
+
+//修改未选中时的图标大小
+        itemView.setShifting(false)
+        itemView.setIconSize(150)
+
+
+
+
+
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_deal, R.id.navigation_upload,R.id.navigation_message,R.id.navigation_me
+                R.id.navigation_home,
+                R.id.navigation_deal,
+                R.id.navigation_upload,
+                R.id.navigation_message,
+                R.id.navigation_me
             )
         )
         navView.setupWithNavController(navController)
+
 
     }
 }
