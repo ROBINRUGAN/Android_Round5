581597cf6b587354f3aa57a29ea894e111a5683f
ROBINRUGAN <1220045015@qq.com>
2023-05-05 20:36:57 +0800

    “我的”页面编写完毕

--- app/src/main/java/com/example/android_round5/ui/me/MeFragment.kt
+++ app/src/main/java/com/example/android_round5/ui/me/MeFragment.kt
@@ -27,11 +27,6 @@ class MeFragment : Fragment() {
 
         _binding = FragmentMeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textMe
-        meViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
